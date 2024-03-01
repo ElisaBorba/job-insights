c592e28193c08f1fa93bd2cfd232b04a173898ab
ElisaBorba <elisaborbaa@gmail.com>
2024-03-01 01:19:16 -0300

    try to fix test 1

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,10 +2,5 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-
-    path = "tests/mocks/mock_counter.csv"
-    word = "python"
-    expected = 7
-
-    result = count_ocurrences(path, word)
-    assert result == expected
+    assert count_ocurrences("data/jobs.csv", "Python") == 1639
+    assert count_ocurrences("data/jobs.csv", "JavaScript") == 122
